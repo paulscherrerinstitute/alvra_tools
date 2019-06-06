9133a10ab8b859aeda0a3c9024621fa976a7fdfd
NichtJens <nichtjens@gmail.com>
2019-06-06 11:25:52 +0200

    added files for setuptools, travis, and conda packaging

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='alvra_tools',
+    version='0.0.1',
+    url="https://github.com/paulscherrerinstitute/alvra_tools",
+    description='Tools for Alvra',
+    author='Paul Scherrer Institute',
+    packages=['alvra_tools']
+)
+
