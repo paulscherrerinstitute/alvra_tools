78a166695f528b118691ad4c956c2a89b17fb7a5
NichtJens <nichtjens@gmail.com>
2019-07-12 18:52:33 +0200

    added ConfigFile class, populate channels.py automatically from local channels.ini or fall back to default alvra_tools/channels.ini

---
+++ alvra_tools/cfgfile.py
@@ -0,0 +1,146 @@
+#!/usr/bin/env python3
+
+from configparser import DuplicateSectionError, MissingSectionHeaderError, ConfigParser
+import os
+
+MODES = list("rwai")
+
+
+from ast import literal_eval
+
+
+import re
+
+RE_CLEAN = re.compile(r'\W|^(?=\d)')
+
+def clean_var_name(s):
+    return re.sub(RE_CLEAN, '_', s)
+
+
+
+class ConfigFile(object):
+    """
+    Simplifying wrapper for ConfigParser
+    Maps a section in a config file to an object with attributes
+    """
+
+    def __init__(self, filename=None, section='GENERAL', mode='i'):
+        """
+        mode can be:
+            'r' (read-only), 'w' ((over)write), 'a' (append), 'i' (insert)
+        """
+        self._filename = filename
+        self._section  = section
+        self._mode     = mode
+
+        if mode not in MODES:
+            raise ValueError("mode string must be one of '{}' or '{}', not '{}'".format(", ".join(MODES[:-1]), MODES[-1], mode))
+
+        self._parser = ConfigParser()
+        if filename:
+            self._read()
+
+
+    def __iter__(self):
+        return iter(self._dict)
+
+    def __getitem__(self, item):
+        return self._dict[item]
+
+    def keys(self):
+        return self._dict.keys()
+
+
+    def _read(self, filename=None, section=None, replace=True):
+        filename = filename or self._filename
+        section  = section  or self._section
+
+        if not os.path.exists(filename):
+            os.listdir(filename) # raise the correct exception
+
+        try:
+            self._parser.read(filename)
+        except MissingSectionHeaderError:
+            with open(filename) as f:
+                content = "[{}]\n".format(section) + f.read()
+                self._parser.read_string(content)
+
+        items = self._parser.items(section)
+        for name, value in items:
+            name = clean_var_name(name)
+            value = literal_eval(value)
+            setattr(self, name, value)
+
+        if replace:
+            self._filename = filename
+            self._section  = section
+
+
+    @property
+    def _dict(self):
+        return {name: value for name, value in sorted(self.__dict__.items())
+                            if not name.startswith("_")}
+
+
+    def write(self, filename=None, section=None, replace=False):
+        filename = filename or self._filename
+        section  = section  or self._section
+
+        mode = self._mode
+        if mode == 'r':
+            return
+
+        outcfg = ConfigParser()
+        if mode == 'i':
+            mode = 'w'
+            outcfg.read(filename)
+
+        try:
+            outcfg.add_section(section)
+        except DuplicateSectionError:
+            pass
+
+        items = self._dict.items()
+        for name, value in items:
+            value = str(value)
+            outcfg.set(section, name, value)
+
+        with open(filename, mode+'b') as outfile:
+            outcfg.write(outfile)
+
+        if replace:
+            self._parser   = outcfg
+            self._filename = filename
+            self._section  = section
+
+
+    def __repr__(self):
+        arg_strings = []
+        for name, value in self._dict.items():
+            arg_strings.append("{}={}".format(name, value))
+
+        type_name = type(self).__name__
+        return "{}({})".format(type_name, ', '.join(arg_strings))
+
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self.write()
+
+
+
+
+
+if __name__ == "__main__":
+    cfg = ConfigFile("channels.ini")
+    length = max(len(i) for i in cfg)
+    for i in cfg:
+        print(i.ljust(length), cfg[i])
+    locals().update(cfg)
+
+
+
+
+
--- alvra_tools/channels.py
+++ alvra_tools/channels.py
@@ -1,60 +1,30 @@
-#detector_name = "JF02T09V01" # this is not used for the read-out channels anymore!
-
-#channel_JF_images      = "{}/data".format(detector_name)
-#channel_JF_pulse_ids   = "{}/pulse_id".format(detector_name)
-
-
-# detector name associated to the current file is inserted in front of these two channels,
-# e.g., "{detector_name}/data" -> "JF02T09V02/data"
-channel_JF_images      = "/data"
-channel_JF_pulse_ids   = "/pulse_id"
+import os
+import logging
+from cfgfile import ConfigFile
 
 
-channel_Events         = "SAR-CVME-TIFALL5:EvtSet/data"                  #Event code channel
+DEFAULT_FNAME = "channels.ini"
 
-channel_delay          = "SLAAR11-LMOT-M451:ENC_1_BS/data"               #Delay of pump-probe delay stage
-channel_delay_NPP      = "SLAAR11-LTIM01-EVR0:DUMMY_PV5_NBS/data"        #Delay in mm from the NPP (spatial encoding TT) small stage
 
-channel_BS_pulse_ids   = "SLAAR11-LMOT-M451:ENC_1_BS/pulse_id"           #Pulse ids taken from pump-probe delay stage (100 Hz)
-channel_PIPS_trans     = "SARES11-GES1:CH1_VAL_GET/data"                 #X-ray TRANS diode to PRIME Keysight (channel 1)
-channel_PIPS_fluo      = "SARES11-GES1:CH2_VAL_GET/data"                 #X-ray FLUO diode to PRIME Keysight (channel 2)
+#logging.basicConfig(level=logging.NOTSET)
+log = logging.getLogger()
 
-#channel_Izero          = "SARES11-LSCP10-FNS:CH2:VAL_GET/data"          #Izero diode to PRIME Ioxos (channel 1)
-#channel_Izero2         = "SARES11-LSCP10-FNS:CH0:VAL_GET/data"          #Izero diode to PRIME Ioxos (channel 0)
-#channel_Izero3         = "SARES11-LSCP10-FNS:CH1:VAL_GET/data"          #Izero diode to PRIME Ioxos (channel 3)
-#channel_Izero4         = "SARES11-LSCP10-FNS:CH2:VAL_GET/data"          #Izero diode to PRIME Ioxos (channel 2)
 
-#channel_Izero          = "SAROP11-CVME-PBPS2:Lnk9Ch12-DATA-SUM/data"     #Izero diode to Wavedream PBPS117 (Up - PDU)
-#channel_Izero2         = "SAROP11-CVME-PBPS2:Lnk9Ch13-DATA-SUM/data"     #Izero diode to Wavedream PBPS117 (Down - PDD)
-#channel_Izero3         = "SAROP11-CVME-PBPS2:Lnk9Ch14-DATA-SUM/data"     #Izero diode to Wavedream PBPS117 (Right - PDR)
-#channel_Izero4         = "SAROP11-CVME-PBPS2:Lnk9Ch15-DATA-SUM/data"     #Izero diode to Wavedream PBPS117 (Left - PDL)
-
-channel_Izero2        = "SAROP11-CVME-PBPS1:Lnk9Ch4-DATA-SUM/data"      #Izero diode to Wavedream PBPS122 (Up - PDU)
-channel_Izero          = "SAROP11-CVME-PBPS1:Lnk9Ch5-DATA-SUM/data"      #Izero diode to Wavedream PBPS122 (Down - PDD)
-channel_Izero3         = "SAROP11-CVME-PBPS1:Lnk9Ch6-DATA-SUM/data"      #Izero diode to Wavedream PBPS122 (Right - PDR)
-channel_Izero2         = "SAROP11-CVME-PBPS1:Lnk9Ch7-DATA-SUM/data"      #Izero diode to Wavedream PBPS122 (Left - PDL)
-
-#channel_Izero          = "SARFE10-PBPG050:HAMP-INTENSITY-CAL/data"        #Izero from the gas monitor
-
-channel_LaserDiode     = "SLAAR11-LSCP1-FNS:CH0:VAL_GET/data"            #Laser diode to ESA Laser Ioxos 
-channel_Laser_refDiode = "SLAAR11-LSCP1-FNS:CH2:VAL_GET/data"            #Laser diode leaking from beampath, to ESA Laser Ioxos 
-channel_Laser_diag     = "SLAAR11-LSCP1-FNS:CH4:VAL_GET/data"            #Laser diode on the DIAG table to laser Ioxos
-
-channel_PALM_eTOF      = "SAROP11-PALMK118:CH2_BUFFER/data"              #PALM eTof from Ch2
-channel_PALM_drift     = "SAROP11-PALMK118:CH2_VAL_GET/data"             #PALM drift
-channel_BAM            = "S10BC01-DBAM070:EOM1_T1/data"                  #BAM arrival time
-
-#channel_laser_yaw      = "SLAAR11-LTIM01-EVR0:DUMMY_PV2_NBS/data"        #Laser mirror rotation Smaract motor
-#channel_laser_pitch    = "SLAAR11-LTIM01-EVR0:DUMMY_PV4_NBS/data"        #Laser mirror pitch Smaract motor
-
-channel_position_X     = "SLAAR11-LTIM01-EVR0:DUMMY_PV1_NBS/data"        #Huber stage X position. 
-channel_position_Y     = "SLAAR11-LTIM01-EVR0:DUMMY_PV2_NBS/data"        #Huber stage Y position. 
-channel_position_Z     = "SLAAR11-LTIM01-EVR0:DUMMY_PV3_NBS/data"        #Huber stage Z position. 
-channel_energy         = "SLAAR11-LTIM01-EVR0:DUMMY_PV4_NBS/data"        #Mono energy in eV
-channel_PALM           = "SLAAR11-LTIM01-EVR0:DUMMY_PV5_NBS/data"        #arrival time in fs
-channel_waveplate       = "SLAAR11-LTIM01-EVR0:DUMMY_PV6_NBS/data"       #LAser waveplate
-#channel_LAM_delaystage = "SLAAR11-LTIM01-EVR0:DUMMY_PV2_NBS/data"        #LAM internal stage position in mm
-#channel_LAM_stepper    = "SLAAR11-LTIM01-EVR0:DUMMY_PV10_NBS/data"       #LAM stepper motor (used for feedback)
+def update_channels(fname=DEFAULT_FNAME):
+    cfg = ConfigFile(fname)
+    globals().update(cfg)
+    log.debug(f"Loaded channels from {fname}")
 
 
 
+try:
+    update_channels()
+except FileNotFoundError:
+    dirname = os.path.dirname(__file__)
+    fname = os.path.join(dirname, DEFAULT_FNAME)
+    update_channels(fname)
+    log.warning(f"Fallback: loaded default channel list ({fname})")
+
+
+
+#TODO handle channels that are deleted from config but are still in globals()
