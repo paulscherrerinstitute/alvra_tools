692d213a474f114e22c1b72340e1a251382b68aa
NichtJens <nichtjens@gmail.com>
2019-07-22 17:52:38 +0200

    removed module load

--- alvra_tools/watcher/runner.sh
+++ alvra_tools/watcher/runner.sh
@@ -7,12 +7,15 @@ mkdir -p logs
 #SBATCH -o logs/test_watcher.%j.out
 
 
-module load psi-python36/4.4.0
+#module load psi-python36/4.4.0
 
 #source /sf/alvra/anaconda/4.4.0/bin/activate 4.4.0
 
 source /sf/photo/miniconda/etc/profile.d/conda.sh
 conda activate dev
+
+#conda list
+#which python3
 
 
 mkdir -p $(dirname $target)
