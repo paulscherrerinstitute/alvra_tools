903432f372b0359febca72c434fcc962abbde9e2
Ivan Usov <ivan.usov@psi.ch>
2020-01-15 12:16:57 +0100

    Remove gain/pedestal loading functions

--- alvra_tools/load_meta.py
+++ alvra_tools/load_meta.py
@@ -1,26 +1,6 @@
-import h5py
 import json
 import numpy as np
 
-
-def load_gain_data(fn):
-    """
-    Load the gains file
-    (usually located in /sf/alvra/config/jungfrau/gainMaps/JF02T09V01/)
-    """
-    with h5py.File(fn, "r") as f:
-        G = f["gains"][:]
-    return G
-
-def load_pede_data(fn):
-    """
-    Load the pedestal file
-    (usually located in /sf/alvra/data/*pgroup*/res/JF_pedestals)
-    """
-    with h5py.File(fn, "r") as f:
-        P = f["gains"][:]
-        pixel_mask = f["pixel_mask"][:]
-    return P, pixel_mask
 
 def load_scan_data(fn):
     with open(fn) as f:
@@ -31,6 +11,3 @@ def load_scan_readback(fn):
     with open(fn) as f:
         data = json.load(f)
     return np.array(data["scan_readbacks"]).ravel()
-
-
-
