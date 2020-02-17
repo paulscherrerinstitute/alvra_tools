71c81b1f25e4a57f54dff15f3e380b9125d6d4df
cirelli_c <claudio.cirelli@psi.ch>
2020-02-17 17:56:13 +0100

    version 0.0.5a, pin to minor version of jungfrau_utils

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='alvra_tools',
-    version='0.0.5',
+    version='0.0.5a',
     url="https://github.com/paulscherrerinstitute/alvra_tools",
     description='Tools for Alvra',
     author='Paul Scherrer Institute',
