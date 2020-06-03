783973106086d01d8ee6336ddb072a6e8d09b155
cirelli_c <claudio.cirelli@psi.ch>
2020-06-03 11:11:33 +0200

    changed to v0.0.8, ready to release it

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='alvra_tools',
-    version='0.0.7',
+    version='0.0.8',
     url="https://github.com/paulscherrerinstitute/alvra_tools",
     description='Tools for Alvra',
     author='Paul Scherrer Institute',
