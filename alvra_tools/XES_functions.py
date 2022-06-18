8e7ebfedc5186549f6d5b9744f3f362ac0ad1966
cirelli_c <claudio.cirelli@psi.ch>
2022-06-18 22:54:24 +0200

    Added delay scans with and without TT

--- alvra_tools/XES_functions.py
+++ alvra_tools/XES_functions.py
@@ -311,7 +311,7 @@ def XES_delayscan_4ROIs(scan, pgroup, roi1, roi2, roi3, roi4, thr_low, thr_high,
     XES_roi3_OFF = []
     XES_roi4_OFF = []
 
-    for i, step in enumerate(scan.files[:nsteps]):
+    for i, step in enumerate(scan[:nsteps]):
 
         JF_file = [f for f in step if "JF02" in f][0]
 
@@ -346,6 +346,272 @@ def XES_delayscan_4ROIs(scan, pgroup, roi1, roi2, roi3, roi4, thr_low, thr_high,
 
     print ("\nJob done! It took", clock_int.tock(), "seconds to process", len(scan.files), "file(s)")
     return XES_roi1_ON, XES_roi2_ON, XES_roi3_ON, XES_roi4_ON, pids_on, XES_roi1_OFF, XES_roi2_OFF, XES_roi3_OFF, XES_roi4_OFF, pids_off, Delay_fs, Delay_mm
+
+
+######################################
+
+def XES_PumpProbe_4ROIs_sfdata(SFDfile, pgroup, roi1, roi2, roi3, roi4, thr_low, thr_high, nshots, correctFlag, binsize):
+    channels_pp = [channel_Events, 'JF02T09V03']
+    channels_all = channels_pp
+    
+    clock_int = clock.Clock()
+
+    roiarray = [roi1, roi2, roi3, roi4]
+
+    spec_roi1_ON = 0
+    spec_roi2_ON = 0
+    spec_roi3_ON = 0
+    spec_roi4_ON = 0
+
+    spec_roi1_OFF = 0
+    spec_roi2_OFF = 0
+    spec_roi3_OFF = 0
+    spec_roi4_OFF = 0
+
+    #print("Processing file {}".format(scan.files[0][0].split('/')[-1].split('.')[0]))
+
+    results_pp, result_FEL, pids_on, pids_off = load_data_compact_laser_pump_JF_noPair(channels_pp, channels_all, SFDfile, roi1, roi2, roi3, roi4)
+
+    imgs_on_roi1 = results_pp["JFroi1"].pump
+    imgs_on_roi2 = results_pp["JFroi2"].pump
+    imgs_on_roi3 = results_pp["JFroi3"].pump
+    imgs_on_roi4 = results_pp["JFroi4"].pump
+
+    imgs_off_roi1 = results_pp["JFroi1"].unpump
+    imgs_off_roi2 = results_pp["JFroi2"].unpump
+    imgs_off_roi3 = results_pp["JFroi3"].unpump
+    imgs_off_roi4 = results_pp["JFroi4"].unpump
+
+    imgs_on_roi1_thr = threshold(imgs_on_roi1, thr_low, thr_high)
+    imgs_on_roi2_thr = threshold(imgs_on_roi2, thr_low, thr_high)
+    imgs_on_roi3_thr = threshold(imgs_on_roi3, thr_low, thr_high)
+    imgs_on_roi4_thr = threshold(imgs_on_roi4, thr_low, thr_high)
+
+    imgs_on_roi1_thr_sum = np.average(imgs_on_roi1_thr, axis = 0)
+    imgs_on_roi2_thr_sum = np.average(imgs_on_roi2_thr, axis = 0)
+    imgs_on_roi3_thr_sum = np.average(imgs_on_roi3_thr, axis = 0)
+    imgs_on_roi4_thr_sum = np.average(imgs_on_roi4_thr, axis = 0)
+
+    imgs_off_roi1_thr = threshold(imgs_off_roi1, thr_low, thr_high)
+    imgs_off_roi2_thr = threshold(imgs_off_roi2, thr_low, thr_high)
+    imgs_off_roi3_thr = threshold(imgs_off_roi3, thr_low, thr_high)
+    imgs_off_roi4_thr = threshold(imgs_off_roi4, thr_low, thr_high)
+
+    imgs_off_roi1_thr_sum = np.average(imgs_off_roi1_thr, axis = 0)
+    imgs_off_roi2_thr_sum = np.average(imgs_off_roi2_thr, axis = 0)
+    imgs_off_roi3_thr_sum = np.average(imgs_off_roi3_thr, axis = 0)
+    imgs_off_roi4_thr_sum = np.average(imgs_off_roi4_thr, axis = 0)
+
+    image_on_array = [imgs_on_roi1_thr_sum, imgs_on_roi2_thr_sum, imgs_on_roi3_thr_sum, imgs_on_roi4_thr_sum]
+    image_off_array = [imgs_off_roi1_thr_sum, imgs_off_roi2_thr_sum, imgs_off_roi3_thr_sum, imgs_off_roi4_thr_sum]
+
+    correct_array_on = []
+    correct_array_off = []    
+
+    for index in range(len(image_on_array)):
+        if correctFlag[index]:
+            print ('{} will be corrected'.format(roiarray[index]))
+            image_on_off = image_on_array[index] + image_off_array[index]
+            maxvalue = np.max(image_on_off.sum(axis=0))
+            #maxvalue_off = np.max(image_off_array[index].sum(axis=0))
+            refpxl = np.array(find_peaks(image_on_off.sum(axis=0), height=maxvalue/2))[0][0]
+            #refpxl_off = np.array(find_peaks(image_off_array[index].sum(axis=0), height=maxvalue_off/2))[0][0]
+            
+            imgs_correct_on = line_rectifier(image_on_array[index], binsize, refpxl)
+            imgs_correct_off = line_rectifier(image_off_array[index], binsize, refpxl)
+        else:
+            print ('{} will remain as it is'.format(roiarray[index]))
+            imgs_correct_on = image_on_array[index]
+            imgs_correct_off = image_off_array[index]
+        correct_array_on.append(imgs_correct_on)
+        correct_array_off.append(imgs_correct_off)
+
+    spec_roi1_ON  = correct_array_on[0].sum(axis = 0)
+    spec_roi2_ON  = correct_array_on[1].sum(axis = 0)
+    spec_roi3_ON  = correct_array_on[2].sum(axis = 0)
+    spec_roi4_ON  = correct_array_on[3].sum(axis = 0)
+
+    spec_roi1_OFF  = correct_array_off[0].sum(axis = 0)
+    spec_roi2_OFF  = correct_array_off[1].sum(axis = 0)
+    spec_roi3_OFF  = correct_array_off[2].sum(axis = 0)
+    spec_roi4_OFF  = correct_array_off[3].sum(axis = 0)
+    print ('Loaded {} images ON, {} images OFF'.format(imgs_on_roi1.shape[0],imgs_off_roi1.shape[0]))
+    print ("It took", clock_int.tick(), "seconds to process this file")
+    return spec_roi1_ON, spec_roi2_ON, spec_roi3_ON, spec_roi4_ON, pids_on, spec_roi1_OFF, spec_roi2_OFF, spec_roi3_OFF, spec_roi4_OFF, pids_off
+
+######################################
+
+def XES_delayscan_4ROIs_sfdata(scan, pgroup, roi1, roi2, roi3, roi4, thr_low, thr_high, nshots, correctFlag, binsize, nsteps=None):
+    clock_int = clock.Clock()
+
+#    from sfdata import SFScanInfo
+#    scan = SFScanInfo(json_file)
+
+    if ' as delay' in scan.parameters['name'][0]:
+        print ('Scan is done with the stage in fs')
+        Delay_fs = scan.readbacks
+        Delay_mm = fs2mm(scan.readbacks,0)
+    else:
+        print ('Scan is done with the stage in mm')
+        Delay_fs = mm2fs(scan.readbacks,0)
+        Delay_mm = scan.readbacks
+
+    XES_roi1_ON = []
+    XES_roi2_ON = []
+    XES_roi3_ON = []
+    XES_roi4_ON = []
+
+    XES_roi1_OFF = []
+    XES_roi2_OFF = []
+    XES_roi3_OFF = []
+    XES_roi4_OFF = []
+
+    for i, step in enumerate(scan[:nsteps]):
+#    for i, step in enumerate(scan):
+
+        JF_file = [f for f in step if "JF02" in f][0]
+        ff = scan.files[0][0].split('/')[-1].split('.')[0]
+        print("File {} out of {}: {}".format(i+1, int(len(scan.files) if nsteps is None else nsteps), ff))
+
+        spec_roi1_ON, spec_roi2_ON, spec_roi3_ON, spec_roi4_ON, pids_on, \
+        spec_roi1_OFF, spec_roi2_OFF, spec_roi3_OFF, spec_roi4_OFF, pids_off = \
+        XES_PumpProbe_4ROIs_sfdata(step, pgroup, roi1, roi2, roi3, roi4, thr_low, thr_high, nshots, correctFlag, binsize)
+
+        XES_roi1_ON.append(spec_roi1_ON)
+        XES_roi2_ON.append(spec_roi2_ON)
+        XES_roi3_ON.append(spec_roi3_ON)
+        XES_roi4_ON.append(spec_roi4_ON)
+
+        XES_roi1_OFF.append(spec_roi1_OFF)
+        XES_roi2_OFF.append(spec_roi2_OFF)
+        XES_roi3_OFF.append(spec_roi3_OFF)
+        XES_roi4_OFF.append(spec_roi4_OFF)
+
+        clear_output(wait=True)
+        print ("It took", clock_int.tick(), "seconds to process this file")
+
+    XES_roi1_ON = np.asarray(XES_roi1_ON)
+    XES_roi2_ON = np.asarray(XES_roi2_ON)
+    XES_roi3_ON = np.asarray(XES_roi3_ON)
+    XES_roi4_ON = np.asarray(XES_roi4_ON)
+
+    XES_roi1_OFF = np.asarray(XES_roi1_OFF)
+    XES_roi2_OFF = np.asarray(XES_roi2_OFF)
+    XES_roi3_OFF = np.asarray(XES_roi3_OFF)
+    XES_roi4_OFF = np.asarray(XES_roi4_OFF)
+
+    print ("\nJob done! It took", clock_int.tock(), "seconds to process", int(len(scan.files) if nsteps is None else nsteps), "file(s)")
+    return XES_roi1_ON, XES_roi2_ON, XES_roi3_ON, XES_roi4_ON, pids_on, XES_roi1_OFF, XES_roi2_OFF, XES_roi3_OFF, XES_roi4_OFF, pids_off, Delay_fs, Delay_mm
+
+######################################
+
+TT_PSEN126 = [channel_PSEN125_signal, channel_PSEN125_bkg, channel_PSEN125_arrTimes, channel_PSEN125_arrTimesAmp, channel_PSEN125_peaks, channel_PSEN125_edges]
+
+def XES_delayscan_TT_4ROIs(scan, pgroup, TT, channel_delay_motor, timezero_mm, roi1, roi2, roi3, roi4, thr_low, thr_high, nshots, nsteps=None):
+    clock_int = clock.Clock()
+    channels_pp = [channel_Events, channel_delay_motor, 'JF02T09V03'] + TT
+    channels_all = channels_pp
+
+#    from sfdata import SFScanInfo
+#    scan = SFScanInfo(json_file)
+
+    if ' as delay' in scan.parameters['name'][0]:
+        print ('Scan is done with the stage in fs')
+        Delay_fs = scan.readbacks
+        Delay_mm = fs2mm(scan.readbacks,0)
+    else:
+        print ('Scan is done with the stage in mm')
+        Delay_fs = mm2fs(scan.readbacks,0)
+        Delay_mm = scan.readbacks
+
+    Delay_fs_stage = []
+    arrTimes_scan = []
+    arrTimesAmp_scan = []
+    Delays_fs_scan = []
+
+    XES_roi1_ON = []
+    XES_roi2_ON = []
+    XES_roi3_ON = []
+    XES_roi4_ON = []
+
+    XES_roi1_OFF = []
+    XES_roi2_OFF = []
+    XES_roi3_OFF = []
+    XES_roi4_OFF = []
+
+    for i, step in enumerate(scan[:nsteps]):
+
+        JF_file = [f for f in step if "JF02" in f][0]
+
+        print("File {} out of {}:".format(i+1, int(len(scan.files) if nsteps is None else nsteps)))
+
+        resultsPP,_,_,_ = load_data_compact_laser_pump_JF_noPair(channels_pp, channels_all, step, roi1, roi2, roi3, roi4)
+
+        delay_shot = resultsPP[channel_delay_motor].pump
+        delay_shot_fs = mm2fs(delay_shot, timezero_mm)
+        Delay_fs_stage.append(delay_shot_fs.mean())
+
+        arrTimes = resultsPP[channel_PSEN125_arrTimes].pump
+        arrTimesAmp = resultsPP[channel_PSEN125_arrTimesAmp].pump
+        sigtraces = resultsPP[channel_PSEN125_edges].pump
+        peaktraces = resultsPP[channel_PSEN125_peaks].pump
+
+        XES_shot_roi1_ON_thr = threshold(resultsPP["JFroi1"].pump, thr_low, thr_high)
+        XES_shot_roi2_ON_thr = threshold(resultsPP["JFroi2"].pump, thr_low, thr_high)
+        XES_shot_roi3_ON_thr = threshold(resultsPP["JFroi3"].pump, thr_low, thr_high)
+        XES_shot_roi4_ON_thr = threshold(resultsPP["JFroi4"].pump, thr_low, thr_high)
+
+        XES_shot_roi1_OFF_thr = threshold(resultsPP["JFroi1"].unpump, thr_low, thr_high)
+        XES_shot_roi2_OFF_thr = threshold(resultsPP["JFroi2"].unpump, thr_low, thr_high)
+        XES_shot_roi3_OFF_thr = threshold(resultsPP["JFroi3"].unpump, thr_low, thr_high)
+        XES_shot_roi4_OFF_thr = threshold(resultsPP["JFroi4"].unpump, thr_low, thr_high)
+           
+        spec_roi1_ON = XES_shot_roi1_ON_thr.sum(axis=1)
+        spec_roi2_ON = XES_shot_roi2_ON_thr.sum(axis=1)
+        spec_roi3_ON = XES_shot_roi3_ON_thr.sum(axis=1)
+        spec_roi4_ON = XES_shot_roi4_ON_thr.sum(axis=1)
+
+        spec_roi1_OFF = XES_shot_roi1_OFF_thr.sum(axis=1)
+        spec_roi2_OFF = XES_shot_roi2_OFF_thr.sum(axis=1)
+        spec_roi3_OFF = XES_shot_roi3_OFF_thr.sum(axis=1)
+        spec_roi4_OFF = XES_shot_roi4_OFF_thr.sum(axis=1)
+
+        XES_roi1_ON.append(spec_roi1_ON)
+        XES_roi2_ON.append(spec_roi2_ON)
+        XES_roi3_ON.append(spec_roi3_ON)
+        XES_roi4_ON.append(spec_roi4_ON)
+
+        XES_roi1_OFF.append(spec_roi1_OFF)
+        XES_roi2_OFF.append(spec_roi2_OFF)
+        XES_roi3_OFF.append(spec_roi3_OFF)
+        XES_roi4_OFF.append(spec_roi4_OFF)
+
+        Delays_fs_scan.append(delay_shot_fs)
+        arrTimes_scan.append(arrTimes)
+
+        clear_output(wait=True)
+        print ("It took", clock_int.tick(), "seconds to process this file")
+
+    #Delay_mm = Delay_mm[:np.shape(Pump_probe)[0]]
+    #Delay_fs = Delay_fs[:np.shape(Pump_probe)[0]]
+
+    Delays_fs_scan = np.asarray(list(itertools.chain.from_iterable(Delays_fs_scan)))
+    arrTimes_scan = np.asarray(list(itertools.chain.from_iterable(arrTimes_scan)))
+
+    Delays_corr_scan = Delays_fs_scan + arrTimes_scan
+
+    XES_roi1_ON = np.asarray(list(itertools.chain.from_iterable(XES_roi1_ON)))
+    XES_roi2_ON = np.asarray(list(itertools.chain.from_iterable(XES_roi2_ON)))
+    XES_roi3_ON = np.asarray(list(itertools.chain.from_iterable(XES_roi3_ON)))
+    XES_roi4_ON = np.asarray(list(itertools.chain.from_iterable(XES_roi4_ON)))
+    
+    XES_roi1_OFF = np.asarray(list(itertools.chain.from_iterable(XES_roi1_OFF)))
+    XES_roi2_OFF = np.asarray(list(itertools.chain.from_iterable(XES_roi2_OFF)))
+    XES_roi3_OFF = np.asarray(list(itertools.chain.from_iterable(XES_roi3_OFF)))
+    XES_roi4_OFF = np.asarray(list(itertools.chain.from_iterable(XES_roi4_OFF)))
+
+    print ("\nJob done! It took", clock_int.tock(), "seconds to process", int(len(scan.files) if nsteps is None else nsteps), "file(s)")
+    return Delays_fs_scan, Delays_corr_scan, XES_roi1_ON, XES_roi2_ON, XES_roi3_ON, XES_roi4_ON, XES_roi1_OFF, XES_roi2_OFF, XES_roi3_OFF, XES_roi4_OFF, Delay_fs, Delay_mm
 
 
 ######################################
@@ -386,6 +652,61 @@ def RIXS_static_4ROIs(json_file, pgroup, roi1, roi2, roi3, roi4, thr_low, thr_hi
     print ("\nJob done! It took", clock_int.tock(), "seconds to process", len(scan.files), "file(s)")
     return RIXS_roi1, RIXS_roi2, RIXS_roi3, RIXS_roi4, Energy_eV, pids
     
+######################################
+
+def XES_scan_4ROIs_sfdata(scan, pgroup, roi1, roi2, roi3, roi4, thr_low, thr_high, nshots, correctFlag, binsize, nsteps=None):
+    clock_int = clock.Clock()
+
+#    from sfdata import SFScanInfo
+#    scan = SFScanInfo(json_file)
+
+    Adjustable = scan.readbacks
+
+    XES_roi1_ON = []
+    XES_roi2_ON = []
+    XES_roi3_ON = []
+    XES_roi4_ON = []
+
+    XES_roi1_OFF = []
+    XES_roi2_OFF = []
+    XES_roi3_OFF = []
+    XES_roi4_OFF = []
+
+    for i, step in enumerate(scan[:nsteps]):
+
+        ff = scan.files[0][0].split('/')[-1].split('.')[0]
+        print("File {} out of {}: {}".format(i+1, int(len(scan.files) if nsteps is None else nsteps), ff))
+
+        spec_roi1_ON, spec_roi2_ON, spec_roi3_ON, spec_roi4_ON, pids_on, \
+        spec_roi1_OFF, spec_roi2_OFF, spec_roi3_OFF, spec_roi4_OFF, pids_off = \
+        XES_PumpProbe_4ROIs_sfdata(step, pgroup, roi1, roi2, roi3, roi4, thr_low, thr_high, nshots, correctFlag, binsize)
+
+        XES_roi1_ON.append(spec_roi1_ON)
+        XES_roi2_ON.append(spec_roi2_ON)
+        XES_roi3_ON.append(spec_roi3_ON)
+        XES_roi4_ON.append(spec_roi4_ON)
+
+        XES_roi1_OFF.append(spec_roi1_OFF)
+        XES_roi2_OFF.append(spec_roi2_OFF)
+        XES_roi3_OFF.append(spec_roi3_OFF)
+        XES_roi4_OFF.append(spec_roi4_OFF)
+
+        clear_output(wait=True)
+        print ("It took", clock_int.tick(), "seconds to process this file")
+
+    XES_roi1_ON = np.asarray(XES_roi1_ON)
+    XES_roi2_ON = np.asarray(XES_roi2_ON)
+    XES_roi3_ON = np.asarray(XES_roi3_ON)
+    XES_roi4_ON = np.asarray(XES_roi4_ON)
+
+    XES_roi1_OFF = np.asarray(XES_roi1_OFF)
+    XES_roi2_OFF = np.asarray(XES_roi2_OFF)
+    XES_roi3_OFF = np.asarray(XES_roi3_OFF)
+    XES_roi4_OFF = np.asarray(XES_roi4_OFF)
+
+    print ("\nJob done! It took", clock_int.tock(), "seconds to process", int(len(scan.files) if nsteps is None else nsteps), "file(s)")
+    return XES_roi1_ON, XES_roi2_ON, XES_roi3_ON, XES_roi4_ON, pids_on, XES_roi1_OFF, XES_roi2_OFF, XES_roi3_OFF, XES_roi4_OFF, pids_off, Adjustable
+
 ######################################
 
 def RIXS_PumpProbe_4ROIs(json_file, pgroup, roi1, roi2, roi3, roi4, thr_low, thr_high, nshots, correctFlag, binsize):
