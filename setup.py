6e06d880773c82995f944d14813dcc1ada110554
cirelli_c <claudio.cirelli@psi.ch>
2024-06-26 10:59:19 +0200

    Release v0.2.48

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="alvra_tools",
-    version='0.2.47',
+    version='0.2.48',
     url="https://github.com/paulscherrerinstitute/alvra_tools",
     description="Tools for Alvra",
     author="Paul Scherrer Institute",
